ed3073b77f52204437d5bb8af6e5e84b08d301b2
maryam ahmadi <mrymrghb82@gmail.com>
2025-06-14 13:43:46 +0330

    Update swagger-setup.js

--- swagger-setup.js
+++ swagger-setup.js
@@ -5,9 +5,14 @@ const swaggerDefinition = {
   openapi: "3.0.0",
   info: {
     title: "API REST Geo Swagger Node.js",
-    description: "Geo simple for save coordinates",
+    description: "RESTful API backend for Geo",
+    version: "1.0.0",
   },
   servers: [
+    {
+      url: "http://185.255.88.110:1766",
+      description: "IP Local server",
+    },
     {
       url: "http://localhost:5400",
       description: "Local server",
